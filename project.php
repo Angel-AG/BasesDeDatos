9e56f95054ed6a432b7453d0d6c5dd7f01399d7c
Angel-AG <miniprobecat@hotmail.com>
2020-06-12 23:57:20 -0500

    Show Project page

--- project.php
+++ project.php
@@ -95,12 +95,14 @@
 
               echo '<div class="card mb-3"><div class="card-body">';
               echo '<h4 class="card-title">' .$row["Nombre"]. '</h4>';
-              echo '<p class="card-text '.$row["Dificultad"].'">' .$row["Dificultad"]. '.</p>';
+              echo '<p class="card-text">De: ' .$row["userName"]. ' ' .$row["Apellido"]. ' | Creado el ' .$row["Fecha_de_creacion"]. '</p>';
+              echo '<p class="lead">' .$row["Descripcion"].'</p>';
               echo '<hr class="my-3">';
               echo '<div class="row">';
               echo '<div class="col-4"><h5 style="color: GoldenRod;">Estrellas: ' .$row["TotalEstrellas"]. '</h5></div>';
               if ($row["sumaCali"]) echo '<div class="col-5"><h5>Sin calificación</h5></div>';
               else echo '<div class="col-5"><h5>Calificación general: $' .$row["sumaCali"]. '</h5></div>';
+              echo '<div class="col-3"><h5  class="card-text '.$row["Dificultad"].'">' .$row["Dificultad"]. '</h5></div>';
               echo '</div>';
               echo '</div></div>';
               
@@ -112,10 +114,109 @@
           }
           else {
             $err = "Dificultades técnicas, intente después";
-          } 
+          }
+
+          // Lista de componentes
+          if ($retrieveComp = $conn->prepare("SELECT comp.Nombre, proyComp.Cantidad
+                                              FROM proyecto_componente AS proyComp
+                                              JOIN componentes AS comp ON comp.ID_Componente = proyComp.Componente
+                                              WHERE proyComp.Proyecto = ?;")) {
+            $retrieveComp->bind_param("i", $IDProy);
+            $retrieveComp->execute();
+
+            echo "<h3>Lista de componentes</h3>";
+            $breakRow = 0;
+            $result = $retrieveComp->get_result();
+            while ($row = $result->fetch_assoc()) {
+              if ($breakRow == 0) echo '<div class="card-deck">'; 
+
+              echo '<div class="card mb-3"><div class="card-body">';
+              echo '<div class="row">';
+              echo '<div class="col-10"><h5>' .$row["Nombre"]. '</h5></div>';
+              echo '<div class="col-2"><h5>x' .$row["Cantidad"]. '</h5></div>';
+              echo '</div>';
+              echo '</div></div>';
+              
+              $breakRow = ($breakRow + 1);
+              if ($breakRow == 3) echo '</div>';
+              $breakRow = $breakRow % 3;
+            }
+            if ($breakRow != 0) echo '</div>';
+          }
+          else {
+            $err = "Dificultades técnicas, intente después";
+          }
+          
+          // 
+          if (isset($_SESSION["userID"]) && 
+              $retrieveComp = $conn->prepare("SELECT Autor 
+                                              FROM proyecto 
+                                              WHERE ID_Proyecto = ? AND Autor = ?;")) {
+            $retrieveComp->bind_param("ii", $IDProy, $_SESSION["userID"]);
+            $retrieveComp->execute();
+
+            $result = $retrieveComp->get_result();
+            if ($result->num_rows == 1) {
+              echo '<div class="card"><div class="card-body">';
+              echo '<h4 class="card-title">Agregar componentes a tu proyecto</h4>';
+              echo '<form>
+                      <div class="form-group">
+                        <label for="addComp">Agregar: </label>
+                        <select class="form-control" id="addComp" name="addComp">';
+              if ($optionsComp = $conn->prepare("SELECT ID_Componente, Nombre
+                                                FROM componentes;")) {
+                $optionsComp->execute();
+
+                $result = $optionsComp->get_result();
+                while ($row = $result->fetch_assoc()) {
+                  echo '<option value="'.$row["ID_Componente"].'">'.$row["Nombre"].'</option>';
+                }
+              }
+              echo  '   </select>
+                      </div>';
+              echo '<div class="form-row">
+                      <div class="form-group col">
+                          <input type="number" id="quantity" min="1" class="form-control" name="quantity" placeholder="Cantidad">
+                      </div>
+                      <div class="form-group col">
+                        <button type="submit" class="btn btn-primary btn-block">Agregar</button>
+                      </div>
+                    </div>';
+              echo '</form>';
+              echo '</div></div>';
+            }
+            else {
+              echo '<div class="card"><div class="card-body"><div class="row">';
+              echo '<div class="col"><form>
+                      <div class="form-group">
+                        <button type="submit" name="fav" value="'.$IDProy.'" class="btn btn-primary btn-primary btn-block">Agregar a favoritos</button>
+                      </div></form></div>';
+              echo '<div class="col"><form><div class="form-row">
+                      <div class="form-group col">
+                          <input type="number" id="quantity" min="0" max="10" class="form-control" name="quantity" placeholder="Calificacion: 0 - 10">
+                      </div>
+                      <div class="form-group col">
+                        <button type="submit" name="fav" value="'.$IDProy.'" class="btn btn-primary btn-info btn-block">Calificar</button>
+                      </div></div></form></div>';
+              echo '</div></div></div>';
+              // if ($optionsComp = $conn->prepare("SELECT ID_Componente, Nombre
+              //                                   FROM componentes;")) {
+              //   $optionsComp->execute();
+
+              //   $result = $optionsComp->get_result();
+              //   while ($row = $result->fetch_assoc()) {
+              //     echo '<option value="'.$row["ID_Componente"].'">'.$row["Nombre"].'</option>';
+              //   }
+              // }
+//               INSERT INTO proyectos_favoritos (Proyecto, Usuario)
+// VALUES (3, 55);
+            }
+          }
         }
       ?>
     </div>
+
+    
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
